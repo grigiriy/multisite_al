9bd6be824418b0841ab53d0b32414be685f8d784
Grigiriy <grigiriy.malyshev@gmail.com>
2020-07-19 12:43:49 +0300

    advantages mobile

--- src/js/main.js
+++ src/js/main.js
@@ -30,3 +30,34 @@ $('#types')
       },
     ],
   });
+
+$('#advantages')
+  .find('.slick')
+  .slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    mobileFirst: true,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: 'unslick',
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 540,
+        settings: {
+          slidesToShow: 2,
+          initialSlide: 1,
+        },
+      },
+    ],
+  });
