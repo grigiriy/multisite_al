1b6b30de8412359966be5b7f4422e369b7e4bc59
Grigiriy <grigiriy.malyshev@gmail.com>
2020-07-29 21:00:42 +0300

    links on type cards

--- theme-helpers/template-parts/types.php
+++ theme-helpers/template-parts/types.php
@@ -14,7 +14,7 @@
             <div class="row row-cols-lg-5 row-cols-1 slick">
                 <div class="col">
                     <div class="rounded-xl py-3 my-3 my-lg-0 shadow">
-                        <a href="/legkovye-avto/" class="d-flex flex-column">
+                        <a href="/pod-pts/" class="d-flex flex-column">
                             <img src="<?= get_with_path('img/card_car.png');?>" alt="" class="mw-100 mb-3">
                             <div class="d-flex text-main subtitle semibold pl-3">
                                 <p class="mt-auto mb-0">
@@ -26,7 +26,7 @@
                 </div>
                 <div class="col">
                     <div class="rounded-xl py-3 my-3 my-lg-0 shadow">
-                        <a href="/gruzovye-avto/" class="d-flex flex-column">
+                        <a href="/gruzovyh-avtomobilej/" class="d-flex flex-column">
                             <img src="<?= get_with_path('img/card_truck.png');?>" alt="" class="mw-100 mb-3">
                             <div class="d-flex text-main subtitle semibold pl-3">
                                 <p class="mt-auto mb-0">
@@ -38,7 +38,7 @@
                 </div>
                 <div class="col">
                     <div class="rounded-xl py-3 my-3 my-lg-0 shadow">
-                        <a href="" class="d-flex flex-column">
+                        <a href="/motociklov/" class="d-flex flex-column">
                             <img src="<?= get_with_path('img/card_moto.png');?>" alt="" class="mw-100 mb-3">
                             <div class="d-flex text-main subtitle semibold pl-3">
                                 <p class="mt-auto mb-0">
@@ -50,7 +50,7 @@
                 </div>
                 <div class="col">
                     <div class="rounded-xl py-3 my-3 my-lg-0 shadow">
-                        <a href="/specztehnika/" class="d-flex flex-column">
+                        <a href="/spectekhniki/" class="d-flex flex-column">
                             <img src="<?= get_with_path('img/card_spec.png');?>" alt="" class="mw-100 mb-3">
                             <div class="d-flex text-main subtitle semibold pl-3">
                                 <p class="mt-auto mb-0">
@@ -62,7 +62,7 @@
                 </div>
                 <div class="col">
                     <div class="rounded-xl py-3 my-3 my-lg-0 shadow">
-                        <a href="/kredity-dlya-yur-licz-i-ip/" class="d-flex flex-column">
+                        <a href="/kredity-dlya-ip-i-yuridicheskih-lic-pod-zalog-pts/" class="d-flex flex-column">
                             <img src="<?= get_with_path('img/card_business.png');?>" alt="" class="mw-100 mb-3">
                             <div class="d-flex text-main subtitle semibold pl-3">
                                 <p class="mt-auto mb-0">
@@ -71,14 +71,6 @@
                             </div>
                         </a>
                     </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 mt-0 mt-lg-5 pt-3 pb-lg-5">
-                    Кредитование без сдачи автомобиля в Москве – это возможность получить крупную сумму под низкий
-                    процент, не жертвуя при этом собственным комфортом. В случае, когда автозалог оформляется под
-                    паспорт транспортного средства, машина остается у хозяина. Он может пользоваться автомобилем без
-                    ограничений для собственных нужд и использовать в бизнесе.
                 </div>
             </div>
         </div>
