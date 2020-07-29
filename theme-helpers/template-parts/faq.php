5f0ae8ffdeb898025a0f7bfe32122a58bd1bc23c
Grigiriy <grigiriy.malyshev@gmail.com>
2020-07-29 21:00:12 +0300

    lil layout fix

--- theme-helpers/template-parts/faq.php
+++ theme-helpers/template-parts/faq.php
@@ -12,11 +12,14 @@
                 </div>
             </div>
             <div class="row">
+
+                <?php foreach( carbon_get_post_meta($post->ID,'faq_text') as $key=>$item) { ?>
+
                 <div class="col-12 accordion" id="faq_accordion">
                     <div class="mb-3">
-                        <div class="rounded-xl bg-lightblue" id="headingOne">
-                            <div class="collapsed d-flex" data-toggle="collapse" data-target="#collapseOne"
-                                aria-expanded="false" aria-controls="collapseOne">
+                        <div class="rounded-xl bg-lightblue" id="heading_<?=$key; ?>">
+                            <div class="collapsed d-flex" data-toggle="collapse" data-target="#collapse_<?=$key; ?>"
+                                aria-expanded="false" aria-controls="#collapse_<?=$key; ?>">
                                 <div class="rounded-circle h-100 bg-white shadow p-2 p-lg-3">
                                     <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
                                         xmlns="http://www.w3.org/2000/svg">
@@ -27,109 +30,24 @@
                                     </svg>
                                 </div>
                                 <span class="pl-3 text-left">
-                                    Автомобиль остается в пользовании кредитополучателя
+                                <?= apply_filters( 'the_content', wpautop( $item['subtitle']  ) ); ?>
                                 </span>
                             </div>
 
                         </div>
 
-                        <div id="collapseOne" class="collapse" aria-labelledby="headingOne"
+                        <div id="collapse_<?=$key; ?>" class="collapse" aria-labelledby="heading_<?=$key; ?>"
                             data-parent="#faq_accordion">
                             <p class="bg-lightblue rounded-xl py-4 px-5">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
-                                squid.
-                                3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa
-                                nesciunt
-                                laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
-                                single-origin
-                                coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-                                wes
-                                anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.
-                                Leggings
-                                occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably
-                                haven't
-                                heard
-                                of them accusamus labore sustainable VHS.
+                            <?= apply_filters( 'the_content', wpautop( $item['text']  ) ); ?>
                             </p>
                         </div>
                     </div>
-                    <div class="mb-3">
-                        <div class="rounded-xl bg-lightblue" id="headingTwo">
-                            <div class="collapsed d-flex" data-toggle="collapse" data-target="#collapseTwo"
-                                aria-expanded="false" aria-controls="collapseTwo">
-                                <div class="rounded-circle h-100 bg-white shadow p-2 p-lg-3">
-                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <rect x="20" y="9" width="2" height="20" rx="1" transform="rotate(90 20 9)"
-                                            fill="#00B4E3" />
-                                        <rect x="11" y="20" width="2" height="20" rx="1" transform="rotate(-180 11 20)"
-                                            fill="#00B4E3" />
-                                    </svg>
-                                </div>
-                                <span class="pl-3 text-left">
-                                    В автоломбарде можно взять кредит под залог ПТС любой техники
-                                </span>
-                            </div>
-                        </div>
-                        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo"
-                            data-parent="#faq_accordion">
-                            <p class="bg-lightblue rounded-xl py-4 px-5">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
-                                squid.
-                                3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa
-                                nesciunt
-                                laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
-                                single-origin
-                                coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-                                wes
-                                anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.
-                                Leggings
-                                occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably
-                                haven't
-                                heard
-                                of them accusamus labore sustainable VHS.
-                            </p>
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <div class="rounded-xl bg-lightblue" id="headingThree">
-                            <div class="d-flex" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true"
-                                aria-controls="collapseThree">
-                                <div class="rounded-circle h-100 bg-white shadow p-2 p-lg-3">
-                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                        <rect x="20" y="9" width="2" height="20" rx="1" transform="rotate(90 20 9)"
-                                            fill="#00B4E3" />
-                                        <rect x="11" y="20" width="2" height="20" rx="1" transform="rotate(-180 11 20)"
-                                            fill="#00B4E3" />
-                                    </svg>
-                                </div>
-                                <span class="pl-3 text-left">
-                                    Круглосуточный прием заявок на кредиты с залогом ПТС доступен по адресам в Москве
-                                </span>
-                            </div>
-                        </div>
-                        <div id="collapseThree" class="collapse show" aria-labelledby="headingThree"
-                            data-parent="#faq_accordion">
-                            <p class="bg-lightblue rounded-xl py-4 px-5">
-                                Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad
-                                squid.
-                                3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa
-                                nesciunt
-                                laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
-                                single-origin
-                                coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore
-                                wes
-                                anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo.
-                                Leggings
-                                occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably
-                                haven't
-                                heard
-                                of them accusamus labore sustainable VHS.
-                            </p>
-                        </div>
-                    </div>
+                    
                 </div>
+                
+                <?php } ?>
+                    
             </div>
         </div>
     </div>
