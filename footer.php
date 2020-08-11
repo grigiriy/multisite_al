5ade002638ad438a74bb8a2eb163e74ba214f9d3
Grigiriy <grigiriy.malyshev@gmail.com>
2020-08-11 20:19:22 +0300

    lil fixes

--- footer.php
+++ footer.php
@@ -88,21 +88,6 @@ get_template_part('theme-helpers/template-parts/modal','form');
 ?>
 
 <?php wp_footer() ?>
-<!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
-   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-   ym(65958358, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-   });
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/65958358" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
   <script defer="defer" src="<?= get_with_path('js/jquery.min.js');?>"></script>
   <script defer="defer" src="<?= get_with_path('js/bootstrap.bundle.min.js');?>"></script>
   <script defer="defer" src="<?= get_with_path('js/slick.min.js');?>"></script>
--- header.php
+++ header.php
@@ -7,6 +7,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?= wp_kses(apply_filters( 'the_content', get_post_field('_genesis_title') ),'strip'); ?></title>
   	<?php wp_head(); ?>
+  <!-- Yandex.Metrika counter -->
+  <script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(65958358, "init", {
+          clickmap:true,
+          trackLinks:true,
+          accurateTrackBounce:true,
+          webvisor:true
+    });
+  </script>
+  <noscript><div><img src="https://mc.yandex.ru/watch/65958358" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+  <!-- /Yandex.Metrika counter -->
   </head>
   <?php $bodyClass = is_front_page( $post->ID ) ? 'main' : '' ?>
 
