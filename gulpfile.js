8e13c2252a2b4f9c645b06510d668e04f1dcb7f0
Grigiriy <grigiriy.malyshev@gmail.com>
2020-09-07 22:35:11 +0300

    update gulp for images

--- gulpfile.js
+++ gulpfile.js
@@ -8,12 +8,20 @@ const scripts = require('./gulp/tasks/script');
 const script_plugins = require('./gulp/tasks/script_plugins');
 const clean = require('./gulp/tasks/clean');
 const moveAssets = require('./gulp/tasks/moveAssets');
+const images = require('./gulp/tasks/images');
 
 const dev = gulp.parallel(pug2html, styles, scripts, script_plugins);
 
-const build = gulp.series(clean, styles, scripts, script_plugins, moveAssets);
+const build = gulp.series(
+  clean,
+  styles,
+  scripts,
+  script_plugins,
+  images,
+  moveAssets
+);
 
 module.exports.scripts = gulp.series(scripts);
-module.exports.styles = gulp.series(styles, watch);
+module.exports.styles = gulp.series(clean, styles, images, moveAssets, watch);
 module.exports.start = gulp.series(build, dev, serve);
 module.exports.build = build;
